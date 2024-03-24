e4da313ab89309bed53d95f94607138472cc5c93
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-03-25 01:12:04 +0200

    Add project files.

---
+++ VideoGames/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace VideoGames
+{
+    public class Game
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }
+        public int StudioId { get; set; } 
+        public GameStudio Studio { get; set; }
+        public int GenreId { get; set; }
+        public GameGenre Genre { get; set; } 
+        public DateTime ReleaseDate { get; set; } 
+    }
+
+    public class GameStudio
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public ICollection<Game> Games { get; set; }
+    }
+
+    public class GameGenre
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public ICollection<Game> Games { get; set; }
+    }
+}
---
+++ VideoGamesContext/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using VideoGames;
+
+
+namespace VideoGamesContext
+{
+    public class VideoGamesContext : DbContext
+    {
+        public VideoGamesContext()
+        {
+            
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameStudio> GameStudios { get; set; }
+        public DbSet<GameGenre> GameGenres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Server=PECHKA\SQLEXPRESS;Database=VideoGames;Integrated Security=SSPI;TrustServerCertificate=true");
+        }
+    }
+}
